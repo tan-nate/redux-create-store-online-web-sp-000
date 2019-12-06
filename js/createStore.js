ec3f4db472589d1b96b80b076bfebd2588b5ce25
Nate Tan <nktan93@gmail.com>
2019-12-06 21:23:14 +0000

    Automatically backed up by Learn

--- js/createStore.js
+++ js/createStore.js
@@ -1,41 +1,41 @@
 function createStore(reducer) {
   let state;
- 
+
   function dispatch(action) {
     state = reducer(state, action);
     render();
   }
- 
+
   function getState() {
     return state;
   };
- 
+
   return {
     dispatch,
     getState
   };
 };
- 
+
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
     case 'INCREASE_COUNT':
       return { count: state.count + 1 };
- 
+
     default:
       return state;
   }
 }
- 
- 
+
+
 function render() {
   let container = document.getElementById('container');
   container.textContent = store.getState().count;
 };
- 
+
 let store = createStore(reducer) // createStore takes the reducer reducer as an argument
 store.dispatch({ type: '@@INIT' });
 let button = document.getElementById('button');
- 
+
 button.addEventListener('click', () => {
   store.dispatch({ type: 'INCREASE_COUNT' });
 });
